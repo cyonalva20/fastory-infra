293fba317e0f9b702913f82c08e8122273b80848
! Meisther <fabianyneron@gmail.com>
2026-06-27 12:55:42 -0500

    feat(security): habilitar cifrado KMS en bucket S3

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -57,14 +57,15 @@ resource "aws_s3_bucket_lifecycle_configuration" "frontend" {
 # ════════════════════════════════════════════════
 # 4. CIFRADO — Server-Side Encryption
 # ════════════════════════════════════════════════
-# Cifrado automático de todos los objetos con AES-256.
+# Cifrado automático de todos los objetos con AWS KMS (alias/aws/s3).
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = "alias/aws/s3"
     }
   }
 }
