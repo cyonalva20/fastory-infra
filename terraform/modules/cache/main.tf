6dde830fab1a859b225fc14f84733ae66e1afd5a
CarlosYon <lawzzz2004@gmail.com>
2026-07-08 18:07:55 -0500

    style(terraform): formatear codigo despues de merge

--- terraform/modules/cache/main.tf
+++ terraform/modules/cache/main.tf
@@ -32,13 +32,13 @@ resource "aws_elasticache_subnet_group" "main" {
 # Se usa para caché de aplicación y manejo de sesiones.
 
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "${local.name_prefix}-redis"
-  engine               = "redis"
-  node_type            = var.redis_node_type
-  num_cache_nodes      = 1
-  port                 = var.redis_port
-  parameter_group_name = "default.redis7"
-  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  cluster_id               = "${local.name_prefix}-redis"
+  engine                   = "redis"
+  node_type                = var.redis_node_type
+  num_cache_nodes          = 1
+  port                     = var.redis_port
+  parameter_group_name     = "default.redis7"
+  subnet_group_name        = aws_elasticache_subnet_group.main.name
   security_group_ids       = [var.redis_security_group_id]
   snapshot_retention_limit = 7
 
