3aa5f67830e17eadf2cca973ef1f04c23399e200
CarlosYon <lawzzz2004@gmail.com>
2026-07-08 22:31:02 -0500

    fix: corregir advertencia de s3 lifecycle y conflicto de log group

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -51,6 +51,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "frontend" {
   rule {
     id     = "cleanup-noncurrent-versions"
     status = "Enabled"
+    
+    filter {}
 
     noncurrent_version_expiration {
       noncurrent_days = 90
